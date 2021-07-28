4c615aea9005a756ed5fa304266feb6d8baaffa2
rebelloguilherme <rebello.guilherme@gmail.com>
2021-07-27 21:27:55 -0300

    DocumentControl working but without file attach yet

---
+++ Data/Migrations/20210726173107_FirstSetup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DocumentControl.Data.Migrations
+{
+    public partial class FirstSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Document",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Codigo = table.Column<int>(type: "int", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Processo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Categoria = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StatusCadastro = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Document", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Document");
+        }
+    }
+}
---
+++ Models/Document.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentControl.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public int Codigo { get; set; }
+        public string Titulo { get; set; }
+        public string Processo { get; set; }
+        public string Categoria { get; set; }
+        public bool StatusCadastro { get; set; }
+
+        
+
+        public Document()
+        {
+
+        }
+
+    }
+}
